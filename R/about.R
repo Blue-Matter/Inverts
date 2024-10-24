8cb02e8ff6498b81d1b906922824e471246fda1f
tcarruth <tomsinbox@gmail.com>
2024-10-24 15:12:39 -0700

    Remake to allow vignette building

---
+++ R/about.R
@@ -0,0 +1,110 @@
+
+#' Get a summary of OM properties
+#'
+#' A plot and optional list of properties
+#'
+#' @param obj An object of class In or OM or RCMfit
+#' @param xlim Vector c(min, max) of longitudinal range to plot
+#' @param ylim Vector c(min, max) of latitudinal range to plot
+#' @param xfac Positive numerical value (probably above 1) that stretches the white space on the x axis around the plot
+#' @param col Color of shaded area
+#' @param plot.fig Logical Should the figure be plotted>
+#' @param ret.dat Logical Should the summary data be returned?
+#' @param p The quantiles to be calculated e.g. p=c(0.005, 0.995) is the 99 percent interval
+#' @author T. Carruthers
+#' @examples
+#' about(OM.GD.7)
+#' @export
+about = function(obj, xlim = c(-134.5,-125), ylim = c(48, 55.5), xfac=1.7, col="red", plot.fig=T, ret.dat=F, p = c(0.05, 0.95)){
+
+  #old.par <- par(no.readonly = TRUE)
+  catchy = Fd = NA
+
+  if(class(obj)=="OM"){
+    OM=obj
+    if("Data"%in%names(OM@cpars))catchy=OM@cpars$Data@Cat[1,]
+    if("Find"%in%names(OM@cpars))Fd = OM@cpars$Find
+  }
+
+  if(class(obj)=="In"){
+    OM=obj[[2]]
+    catchy = obj[[3]]@Chist
+  }
+
+  if(class(obj)=="RCMfit"){
+    OM=obj@OM
+    catchy=OM@cpars@Data@Cat[1,]
+    Fd = OM@cpars$Find
+  }
+  yrs = OM@CurrentYr-(OM@nyears-1):0
+
+  if(plot.fig){
+    par(mai =rep(0.1,4))
+    xfac = 1.7
+    xd = xlim[2]-xlim[1]
+    yd = ylim[2]-ylim[1]
+    xmid = mean(xlim)
+    allxlim = xmid+c(-1,1) *(xd/2*xfac)
+    plot(1,col="white",axes=F,xlim=allxlim,ylim=ylim);grid()
+    map(xlim = xlim, ylim = ylim, add=T)
+    poly = OM@Misc$mapinfo$poly
+    if(class(poly)=="sfc_multipolygon")plot(poly, add=T, col=col)
+    if(class(poly)=="data.frame")polygon(poly[,1],poly[,2],col=col,border=col)
+    cnt = as.numeric(st_centroid(poly[[1]]))
+  }
+  if(!is.null(OM@cpars))cpars = OM@cpars
+  if(is.null(OM@cpars))cpars = list()
+
+  # OM properties
+  cparnams = names(cpars)
+  onam = c("M","K", "Linf","R0","Depln.")
+  snam = c("M_ageArray","K","Linf","R0","D")
+  unit = c(1,          1,    1, 1E6, 1)
+  sigf = c(3,          2,    1, 2, 2)
+  ns = length(onam)
+  levs = (1:ns)[snam%in%cparnams]
+  for(ss in levs)    if(snam[ss]%in%cparnams) assign(onam[ss], round(quantile(cpars[[snam[ss]]]/unit[ss], p),sigf[ss]))
+  slabs = sapply(levs,function(X,onam,snam){paste(onam[X],"=",get(onam[X])[1],"-",get(onam[X])[2])},snam=snam,onam=onam)
+  yrng = range(yrs)
+  ylab = paste0(yrng[1]," - ", yrng[2])
+
+  if(plot.fig){
+    legend('topright', legend = c(OM@Name, ylab, slabs), bty="n", text.col=c(col,rep('black',length(slabs)+1)), cex=1)
+    # Data properties
+    if(!is.na(catchy[1])){
+      par(fig = c(0,0.38,0,0.25),mai = c(0.4,0.7,0.1,0.05),new=T)
+      plot(yrs,catchy,pch=19,col=col,ylab="",xlab="",ylim=c(0,max(catchy)*1.025));grid(); lines(yrs,catchy,col=col)
+      mtext("Catch",2,line=2.2)
+    }
+
+    if(!is.na(Fd[1])){
+      par(fig = c(0,0.38,0.25,0.5),mai = c(0.4,0.7,0.1,0.05),new=T)
+      Fq = apply(Fd,2,quantile,c(0.05,0.25,0.5,0.75,0.95))
+      matplot(yrs,t(Fq),col="NA",xlab="",ylab="",ylim=c(0,max(Fq)*1.025));grid()
+      coly = makeTransparent(col,50)
+      polygon(c(yrs,rev(yrs)),c(Fq[1,],rev(Fq[5,])),col=coly,border=NA)
+      polygon(c(yrs,rev(yrs)),c(Fq[2,],rev(Fq[4,])),col=coly,border=NA)
+      lines(yrs,Fd[3,],col=col)
+      mtext("Fish. Mort.",2,line=2.2)
+    }
+
+    par(fig=c(0,1,0,1))
+  } # end of plot.fig
+
+
+
+  if(ret.dat){
+    outlist = list()
+    outlist[[1]] = OM@Name
+    outlist[[2]] = yrng
+    names(outlist)[1:2] = c("Name","Years")
+    j =2
+    for(ss in levs){
+      j = j +1
+      outlist[[j]] = get(onam[ss])
+      names(outlist)[j] = onam[ss]
+    }
+    return(outlist)
+  }
+
+}
---
+++ R/avail_objects.R
@@ -0,0 +1,92 @@
+
+#' What objects of this class are available
+#'
+#' Generic class finder Finds objects of the specified class in the global environment or the
+#' Inverts packages.
+#'
+#' @param classy A class of object (character string, e.g. 'In', 'OM', 'MSE', 'MP')
+#' @param package Optional. Names(s) of the package to search for object of class `classy`. String
+#' Default is all `Inverts` packages (Inverts, Inverts.GD, Inverts.MC, Inverts.GSU, Inverts.SC). Always searches the global environment as well.
+#' @param msg Print messages?
+#' @examples
+#' objs("In", msg=FALSE)
+#' @author T. Carruthers
+#' @seealso \link{avail}
+#' @examples
+#' RCM_input_files <- objs("In")
+#' OMs <- objs("OM")
+#' @export
+objs = function (classy, package = NULL, msg = TRUE) {
+  temp <- try(class(classy), silent = TRUE)
+  if (methods::is(temp, "try-error"))
+    classy <- deparse(substitute(classy))
+  if (temp == "function")
+    classy <- deparse(substitute(classy))
+  else {
+    packages <- c("Inverts", "Inverts.GD", "Inverts.MC", "Inverts.GSU", "Inverts.SC")
+    if (is.null(package)) {
+      package <- packages
+      pkgs <- search()
+      search_package <- paste0("package:", package)
+      package <- package[search_package %in% pkgs]
+    }
+    global_funs <- ls(envir = .GlobalEnv)[vapply(ls(envir = .GlobalEnv),
+                                                 getclass.inverts, logical(1), classy = classy)]
+    temp <- global_funs
+
+    if ('Inverts' %in% package) {
+      MSEtool_funs <- getfuncs.inverts('Inverts', classy, msg)
+      temp <- c(temp, MSEtool_funs)
+    }
+    if ('Inverts.GD' %in% package) {
+      SAMtool_funs <- getfuncs.inverts('Inverts.GD', classy, msg)
+      temp <- c(temp, SAMtool_funs)
+    }
+    if ('Inverts.MC' %in% package) {
+      DLMtool_funs <- getfuncs.inverts('Inverts.MC', classy, msg)
+      temp <- c(temp, DLMtool_funs)
+    }
+    if ('Inverts.GSU' %in% package) {
+      MSEextra_funs <- getfuncs.inverts('Inverts.GSU', classy, msg)
+      temp <- c(temp, MSEextra_funs)
+    }
+    if ('Inverts.SC' %in% package) {
+      MSEextra_funs <- getfuncs.inverts('Inverts.SC', classy, msg)
+      temp <- c(temp, MSEextra_funs)
+    }
+
+    packagex <- package[!package %in% packages]
+    if (length(packagex) > 0) {
+      other <- sapply(1:length(packagex), function(i) get_funcs(packagex[i],
+                                                                classy, msg))
+      other <- unlist(other)
+      temp <- c(temp, other)
+    }
+    if (length(temp) < 1)
+      stop("No objects of class '", classy, "' found",
+           call. = FALSE)
+    return(unique(temp))
+  }
+}
+
+getclass.inverts = function (x, classy){
+  return(any(class(get(x)) == classy))
+}
+
+getfuncs.inverts = function (package, classy, msg){
+  pkgs <- search()
+  search_package <- paste0("package:", package)
+  funs <- NULL
+  if (search_package %in% pkgs) {
+    if (msg)
+      message("Searching for objects of class ", classy,
+              " in package: ", package)
+    funs <- ls(search_package)[vapply(ls(search_package),
+                                      getclass.inverts, logical(1), classy = classy)]
+  }
+  else {
+    stop("Package ", package, " not loaded. Use `library(",
+         package, ")`", call. = FALSE)
+  }
+  funs
+}
