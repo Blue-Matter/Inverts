e81efb2efd79e43f7c8f42fc769bafecfafea1fd
tcarruth <tomsinbox@gmail.com>
2024-10-25 16:17:53 -0700

    update to concise polys

--- R/about.R
+++ R/about.R
@@ -11,11 +11,13 @@
 #' @param plot.fig Logical Should the figure be plotted>
 #' @param ret.dat Logical Should the summary data be returned?
 #' @param p The quantiles to be calculated e.g. p=c(0.005, 0.995) is the 99 percent interval
+#' @param plot.MAs Logical, should the management areas (statistical areas) be plotted (slower)?
 #' @author T. Carruthers
 #' @examples
 #' about(OM.GD.7)
 #' @export
-about = function(obj, xlim = c(-134.5,-125), ylim = c(48, 55.5), xfac=1.7, col="red", plot.fig=T, ret.dat=F, p = c(0.05, 0.95)){
+about = function(obj, xlim = c(-134.5,-125), ylim = c(48, 55.5), xfac=1.7, col="red",
+                 plot.fig=T, ret.dat=F, p = c(0.05, 0.95), plot.MAs=T){
 
   #old.par <- par(no.readonly = TRUE)
   catchy = Fd = NA
@@ -39,6 +41,7 @@ about = function(obj, xlim = c(-134.5,-125), ylim = c(48, 55.5), xfac=1.7, col="
   yrs = OM@CurrentYr-(OM@nyears-1):0
 
   if(plot.fig){
+
     par(mai =rep(0.1,4))
     xfac = 1.7
     xd = xlim[2]-xlim[1]
@@ -46,11 +49,19 @@ about = function(obj, xlim = c(-134.5,-125), ylim = c(48, 55.5), xfac=1.7, col="
     xmid = mean(xlim)
     allxlim = xmid+c(-1,1) *(xd/2*xfac)
     plot(1,col="white",axes=F,xlim=allxlim,ylim=ylim);grid()
-    map(xlim = xlim, ylim = ylim, add=T)
+
     poly = OM@Misc$mapinfo$poly
-    if(class(poly)=="sfc_multipolygon")plot(poly, add=T, col=col)
-    if(class(poly)=="data.frame")polygon(poly[,1],poly[,2],col=col,border=col)
-    cnt = as.numeric(st_centroid(poly[[1]]))
+
+    if(class(poly)[1]=="data.frame"){
+      polygon(poly[,1],poly[,2],col=col,border=col)
+    }else{ #  if(class(poly)[2]=="sfc")
+      plot(poly, add=T, col=col)
+    }
+
+    if(plot.MAs)plot(MA_polys,add=T)
+
+    map(xlim = xlim, ylim = ylim, add=T,fill=T,col=rep("lightgrey",100))
+    #cnt = as.numeric(st_centroid(poly[[1]]))
   }
   if(!is.null(OM@cpars))cpars = OM@cpars
   if(is.null(OM@cpars))cpars = list()
@@ -69,29 +80,29 @@ about = function(obj, xlim = c(-134.5,-125), ylim = c(48, 55.5), xfac=1.7, col="
   ylab = paste0(yrng[1]," - ", yrng[2])
 
   if(plot.fig){
+    wtbg = function()polygon(c(-1E10,1E10,1E10,-1E10),c(-1E10,-1E10,1E10,1E10),col="white",border="white")
     legend('topright', legend = c(OM@Name, ylab, slabs), bty="n", text.col=c(col,rep('black',length(slabs)+1)), cex=1)
     # Data properties
     if(!is.na(catchy[1])){
       par(fig = c(0,0.38,0,0.25),mai = c(0.4,0.7,0.1,0.05),new=T)
-      plot(yrs,catchy,pch=19,col=col,ylab="",xlab="",ylim=c(0,max(catchy)*1.025));grid(); lines(yrs,catchy,col=col)
+      plot(yrs,catchy,pch=19,col="white",ylab="",xlab="",ylim=c(0,max(catchy)*1.025)); wtbg()
+      points(yrs,catchy,pch=19);grid(); lines(yrs,catchy,col=col)
       mtext("Catch",2,line=2.2)
     }
 
     if(!is.na(Fd[1])){
       par(fig = c(0,0.38,0.25,0.5),mai = c(0.4,0.7,0.1,0.05),new=T)
       Fq = apply(Fd,2,quantile,c(0.05,0.25,0.5,0.75,0.95))
-      matplot(yrs,t(Fq),col="NA",xlab="",ylab="",ylim=c(0,max(Fq)*1.025));grid()
+      matplot(yrs,t(Fq),col="NA",xlab="",ylab="",ylim=c(0,max(Fq)*1.025));wtbg();grid()
       coly = makeTransparent(col,50)
       polygon(c(yrs,rev(yrs)),c(Fq[1,],rev(Fq[5,])),col=coly,border=NA)
       polygon(c(yrs,rev(yrs)),c(Fq[2,],rev(Fq[4,])),col=coly,border=NA)
-      lines(yrs,Fd[3,],col=col)
+      lines(yrs,Fq[3,],col=col)
       mtext("Fish. Mort.",2,line=2.2)
     }
 
     par(fig=c(0,1,0,1))
   } # end of plot.fig
-
-
 
   if(ret.dat){
     outlist = list()
